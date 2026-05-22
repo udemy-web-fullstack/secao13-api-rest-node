e4d8f84a99c7b883e921e3339d9791c65f20db09
magno v gomes <magnovgomes74@gmail.com>
2026-05-22 15:44:13 -0300

    "feat: finaliza o CRUD completo de usuarios com validacoes profissionais"

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,8 +4,70 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-
       return res.json(novoUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors
+          ? e.errors.map((err) => err.message)
+          : ['Erro interno no servidor'],
+      });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch {
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          errors: ['Usuário não encontrado.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors
+          ? e.errors.map((err) => err.message)
+          : ['Erro interno no servidor'],
+      });
+    }
+  }
+  //Delete
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          errors: ['Usuário não encontrado.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
     } catch (e) {
       return res.status(400).json({
         errors: e.errors
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,10 @@ import userController from '../controllers/userController.js';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
 
